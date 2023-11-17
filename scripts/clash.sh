83b15552c5fd985d8c1cb9a00b3392cfb9343fe4
Sunhhb <82243798+SunHHB@users.noreply.github.com>
2023-11-17 11:35:27 +0800

    Update clash.sh

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -4,7 +4,7 @@ echo "Start Clash Core Download !"
 echo "Current Path: $PWD"
 
 mkdir -p files/etc/openclash/core
-cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
+
 
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
 CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep amd64 | awk -F '"' '{print $4}')
