425633c0a5cf1226b9ce353464485708d5b2ed9e
sunhhb <82243798+SunHHB@users.noreply.github.com>
2023-12-06 15:23:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,6 +31,8 @@ PACKAGES="$PACKAGES luci-app-fileassistant"
 PACKAGES="$PACKAGES luci-lib-docker luci-i18n-dockerman-zh-cn"
 # alsit 组件
 PACKAGES="$PACKAGES alist luci-i18n-alist-zh-cn"
+# mosdns 组件
+PACKAGES="$PACKAGES luci-i18n-mosdns-zh-cn"
 # XUNLEI组件
 # PACKAGES="$PACKAGES libc6-compat xunlei luci-app-xunlei luci-i18n-xunlei-zh-cn"
 # 宽带监控 Nlbwmon
