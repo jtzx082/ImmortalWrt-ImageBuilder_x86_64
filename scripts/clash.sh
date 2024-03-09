6d9a74a0db948e8add382c7b60415f2bfe92c30f
sunhhb <82243798+SunHHB@users.noreply.github.com>
2024-03-09 13:24:46 +0800

    Update clash.sh

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -10,9 +10,9 @@ CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master
 CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep amd64 | awk -F '"' '{print $4}')
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
 
-CLASH_DEV_URL2="https://github.com/SunHHB/ShellCrash/raw/master/bin/clash/clash-linux-amd64"
-CLASH_TUN_URL2="https://github.com/SunHHB/ShellCrash/raw/master/bin/clashpre/clash-linux-amd64"
-CLASH_META_URL2="https://github.com/SunHHB/ShellCrash/raw/master/bin/meta/clash-linux-amd64"
+CLASH_DEV_URL2="https://github.com/SunHHB/ShellCrash/blob/master/bin/meta/clash-linux-amd64.tar.gz"
+CLASH_TUN_URL2="https://github.com/SunHHB/ShellCrash/blob/master/bin/clashpre/clash-linux-amd64.tar.gz"
+CLASH_META_URL2="https://github.com/SunHHB/ShellCrash/blob/master/bin/meta/clash-linux-amd64.tar.gz"
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
@@ -27,9 +27,10 @@ GEO_META="https://github.com/MetaCubeX/meta-rules-dat/raw/release/geoip.metadb"
 # wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 # wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 
-wget -qO- $CLASH_DEV_URL2 > files/etc/openclash/core/clash
-wget -qO- $CLASH_META_URL2 > files/etc/openclash/core/clash_meta
-wget -qO- $CLASH_TUN_URL2 > files/etc/openclash/core/clash_tun
+wget -qO- $CLASH_DEV_URL2 | tar xOvz > files/etc/openclash/core/clash
+wget -qO- $CLASH_TUN_URL2 | tar xOvz >  files/etc/openclash/core/clash_tun
+wget -qO- $CLASH_META_URL2 | tar xOvz >  files/etc/openclash/core/clash_meta
+
 
 wget -qO- $GEO_IP > files/etc/openclash/GeoIP.dat
 wget -qO- $GEO_SITE > files/etc/openclash/GeoSite.dat
