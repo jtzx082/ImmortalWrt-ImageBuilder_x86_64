a1c23a3591c7cf249f33d8a5bb98eac86e81f10d
sunhhb <82243798+SunHHB@users.noreply.github.com>
2024-03-09 15:28:52 +0800

    Update clash.sh

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -29,12 +29,12 @@ curl -sfL -o GeoIP.metadb $GEO_META
 
 mkdir ./core/ && cd ./core/
 
-curl -sfL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash clash_meta
-curl -sfL -o tun.tar.gz $CORE_TUN && tar -zxf tun.tar.gz && mv -f clash clash_tun
-curl -sfL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz
+curl -sfL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f clash-linux-amd64 clash_meta
+curl -sfL -o tun.tar.gz $CORE_TUN && tar -zxf tun.tar.gz && mv -f clash-linux-amd64 clash_tun
+curl -sfL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && mv -f clash-linux-amd64 clash
 
 chmod +x ./clash* && rm -rf ./*.gz
-tun
+
 echo "openclash date has been updated!"
 
 exit 0
