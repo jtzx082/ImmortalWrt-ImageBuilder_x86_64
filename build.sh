3ab4e3068fe820d9141e9afc9ca3406cb576f59a
sunhhb <82243798+SunHHB@users.noreply.github.com>
2024-04-23 19:10:10 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,6 @@ PACKAGES="$PACKAGES luci-i18n-autoreboot-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-uugamebooster-zh-cn"
 PACKAGES="$PACKAGES luci-app-cloudflared"
 PACKAGES="$PACKAGES luci-i18n-n2n-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-wireguard-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-socat-zh-cn"
 # upnp
 PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
