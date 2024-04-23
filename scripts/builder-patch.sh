15d8813733d113da7f5c3500e5f099fb62031f66
sunhhb <82243798+SunHHB@users.noreply.github.com>
2024-04-23 16:32:34 +0800

    Update builder-patch.sh

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -10,7 +10,7 @@ sed -i "/luci-app-cpufreq/d" include/target.mk
 
 
 # Force opkg to overwrite files
-# sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
+sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
 
 # Not generate ISO images for it is too big
 sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
