cb76eb3b7fc449eb6511a86f709ff74e38252ffe
sunhhb <82243798+SunHHB@users.noreply.github.com>
2024-05-02 11:58:07 +0800

    Update clash.sh

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -9,6 +9,7 @@ mkdir -p files/etc/openclash && cd files/etc/openclash
 CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
 CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep amd64 | awk -F '"' '{print $4}')
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
+CLASH_META_URL2="https://github.com/SunHHB/ShellCrash/raw/master/bin/meta/clash-linux-amd64"
 
 CORE_DEV="https://github.com/SunHHB/ShellCrash/raw/master/bin/clash/clash-linux-amd64.tar.gz"
 CORE_TUN="https://github.com/SunHHB/ShellCrash/raw/master/bin/clashpre/clash-linux-amd64.tar.gz"
@@ -32,7 +33,8 @@ mkdir ./core/ && cd ./core/
 # curl -sfL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f CrashCore clash_meta
 # curl -sfL -o tun.tar.gz $CORE_TUN && tar -zxf tun.tar.gz && mv -f clash-linux-amd64 clash_tun
 # curl -sfL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && mv -f clash-linux-amd64 clash
-wget -qO- $CORE_MATE | tar xOvz > clash_meta
+wget -qO- $CLASH_META_URL2 > clash_meta
+#wget -qO- $CORE_MATE | tar xOvz > clash_meta
 wget -qO- $CORE_TUN | tar xOvz > clash_tun
 wget -qO- $CORE_DEV | tar xOvz > clash
 
