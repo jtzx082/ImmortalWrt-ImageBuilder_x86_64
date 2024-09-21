521e453971a55265ad4c943bf07f389cb81f9479
sunhhb <82243798+SunHHB@users.noreply.github.com>
2024-09-21 10:13:50 +0800

    clash.sh

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -6,13 +6,9 @@ echo "Current Path: $PWD"
 mkdir -p files/etc/openclash && cd files/etc/openclash
 
 
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz"
-CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep amd64 | awk -F '"' '{print $4}')
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
-CLASH_META_URL2="https://github.com/SunHHB/ShellCrash/raw/master/bin/meta/clash-linux-amd64"
 
-CORE_DEV="https://github.com/SunHHB/ShellCrash/raw/master/bin/clash/clash-linux-amd64.tar.gz"
-CORE_TUN="https://github.com/SunHHB/ShellCrash/raw/master/bin/clashpre/clash-linux-amd64.tar.gz"
+CLASH_META_URL="https://github.com/SunHHB/ShellCrash/raw/master/bin/meta/clash-linux-amd64"
+
 CORE_MATE="https://github.com/SunHHB/ShellCrash/raw/master/bin/meta/clash-linux-amd64.tar.gz"
 
 
@@ -31,12 +27,9 @@ curl -sfL -o GeoIP.metadb $GEO_META
 mkdir ./core/ && cd ./core/
 
 # curl -sfL -o meta.tar.gz $CORE_MATE && tar -zxf meta.tar.gz && mv -f CrashCore clash_meta
-# curl -sfL -o tun.tar.gz $CORE_TUN && tar -zxf tun.tar.gz && mv -f clash-linux-amd64 clash_tun
-# curl -sfL -o dev.tar.gz $CORE_DEV && tar -zxf dev.tar.gz && mv -f clash-linux-amd64 clash
-wget -qO- $CLASH_META_URL2 > clash_meta
 #wget -qO- $CORE_MATE | tar xOvz > clash_meta
-wget -qO- $CORE_TUN | tar xOvz > clash_tun
-wget -qO- $CORE_DEV | tar xOvz > clash
+
+wget -qO- $CLASH_META_URL > clash_meta
 
 chmod +x ./clash* && rm -rf ./*.gz
 
