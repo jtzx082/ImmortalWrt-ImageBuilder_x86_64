69b6db31bbfd8ffc140643392367019e67748864
sunhhb <82243798+SunHHB@users.noreply.github.com>
2024-11-19 19:08:46 +0800

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -28,6 +28,11 @@ PACKAGES="$PACKAGES tailscale"
 PACKAGES="$PACKAGES luci-app-tailscale"
 PACKAGES="$PACKAGES luci-i18n-tailscale-zh-cn"
 
+# 花生壳
+PACKAGES="$PACKAGES phtunnel"
+PACKAGES="$PACKAGES luci-app-phtunnel"
+PACKAGES="$PACKAGES luci-i18n-phtunnel-zh-cn"
+
 # upnp
 PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 # OpenClash 代理
